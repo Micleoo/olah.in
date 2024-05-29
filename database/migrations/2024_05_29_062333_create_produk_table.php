29b79e583ff30241fd69840d42e2b434c01d6a2a
kaylahatnii <kayla@student.ittelkom-sby.ac.id>
2024-05-29 13:59:35 +0700

    memperbaiki bagian produk pada marketplace

---
+++ database/migrations/2024_05_29_062333_create_produk_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk');
+            $table->string('foto_produk')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->integer('harga');
+            $table->integer('jumlah_terjual')->default(0);
+            $table->unsignedBigInteger('id_user');
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produk');
+    }
+}
